fb0d35849f122ce6fae37c26340cf0228df59104
Samuel Lanzuela <amlanzuela@gmail.com>
2023-08-21 13:30:32 -0700

    more things

--- NewBeninging/Assets/Scripts/FinishTrigger.cs
+++ NewBeninging/Assets/Scripts/FinishTrigger.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FinishTrigger : MonoBehaviour
 {
     public GameObject finishText;
+    public TMP_Text timeText;
+    public float timer = 0.000f;
+    bool counting = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,10 @@ public class FinishTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (counting)
+        {
+            timer += Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,6 +29,10 @@ public class FinishTrigger : MonoBehaviour
         if(other.gameObject == GameObject.Find("Player"))
         {
             finishText.SetActive(true);
+            counting = false;
+            timer = Mathf.Round(timer * 1000)/1000;
+            timeText.text = "Time: " + timer;
+            timeText.gameObject.SetActive(true);
         }
     }
 }
